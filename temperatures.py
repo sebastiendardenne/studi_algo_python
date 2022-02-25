25677978ff24b6064974e4681e7e8f2a02431cfe
quillaur <aurelienquillet@gmail.com>
2022-02-25 09:36:49 +0100

    Branch init.

---
+++ temperatures.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of temperatures to analyse
+for i in input().split():
+    # t: a temperature expressed as an integer ranging from -273 to 5526
+    t = int(i)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("result")

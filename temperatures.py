b4daa95569109c542d3d7ef1e45e8638a3573ab7
quillaur <aurelienquillet@gmail.com>
2022-02-25 11:14:40 +0100

    Solution AQ.

--- temperatures.py
+++ temperatures.py
@@ -5,11 +5,29 @@ import math
 # the standard input according to the problem statement.
 
 n = int(input())  # the number of temperatures to analyse
+print("n:", n, file=sys.stderr, flush=True)
+
+closest_0 = 100000
+min_dist = 100000
+
 for i in input().split():
     # t: a temperature expressed as an integer ranging from -273 to 5526
     t = int(i)
+    print("t:", t, file=sys.stderr, flush=True)
+
+    dist_t = abs(t)
+
+    if min_dist > dist_t:
+        closest_0 = t
+        min_dist = dist_t
+    
+    elif min_dist == dist_t:
+        closest_0 = min_dist
+
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-print("result")
+if n > 0:
+    print(f"{closest_0}")
+else:
+    print("0")

42d706ceea0f071232abdd857feae2afca784912
quillaur <aurelienquillet@gmail.com>
2022-02-25 09:39:15 +0100

    Branch init.

---
+++ pertes_bourse.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+for i in input().split():
+    v = int(i)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("answer")
